d973cd1cf851c58eec5dfb3194d754714b107bc8
Mahmoud Mosalman <m.mosalman@gmail.com>
2018-03-07 20:03:00 +0330

    Add links

--- pages/about.vue
+++ pages/about.vue
@@ -3,10 +3,10 @@
     <div class="uk-container">
       <div class="uk-grid">
         <div class="uk-width-1-2">
-          <div class="uk-card uk-card-primary">
-            <h1>Hello Nuxt!</h1>
+          <div class="uk-card uk-card-secondary">
+            <h1>About ...</h1>
           </div>
-          <nuxt-link class="uk-button uk-button-secondary" :to="{name: 'about'}">about</nuxt-link>
+          <nuxt-link class="uk-button uk-button-primary" :to="{name: 'index'}">home</nuxt-link>
         </div>
       </div>
     </div>
